a93964792d84cf28aff17835ee5410a056aa1fa8
DEMENTOR <dmntr@rambler.ru>
2019-04-29 23:39:45 +1000

    hover effects

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 $( ".calendar-header__button-filter, .calendar-filter-block__title-close-button" ).click(function(){
   $( ".calendar-filter-root" ).toggle();
 });
+
+$(".calendar-header__button").hover(function(){
+  //alert('ok');
+});
