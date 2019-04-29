2390efc7dcc12bbdd46073002e2c153cf55be5ad
DEMENTOR <dmntr@rambler.ru>
2019-04-29 23:53:21 +1000

    mobile fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,4 @@
 $( ".calendar-header__button-filter, .calendar-filter-block__title-close-button" ).click(function(){
   $( ".calendar-filter-root" ).toggle();
-});
-
-$(".calendar-header__button").hover(function(){
-  //alert('ok');
+  $( ".calendar-main-block" ).toggle();
 });
